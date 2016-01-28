1c2249ba70d6faaa6fbfff5f72d5b3fd4e251f1e
GRIESS Eric <contact@webroom.fr>
2016-01-28 14:54:45 +0100

    title final

--- app/templates/default/onepage.php
+++ app/templates/default/onepage.php
@@ -8,13 +8,9 @@
 		
 
 	<nav class="navbar navbar-fixed-top">
-<<<<<<< HEAD
 
-		<h1 id="text_shadow" style="text-shadow: rgb(150, 150, 150) 4px 4px 2px;" ><?= $titledisplay[0] ?></h1>
-
-=======
 		<h1 id="text_shadow" style="text-shadow: rgb(150, 150, 150) 4px 4px 2px; color:<?= $color[0] ?>;font-family:<?= $font[0] ?>" ><?= $titledisplay[0] ?></h1>
->>>>>>> header01
+
 		<!-- POP UP edit end -->
 		<ul class="nav nav-pills">
 		  <li role="presentation"><a href="/login">Loginpage</a></li>
